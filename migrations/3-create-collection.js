1e2fce2de8a77205adaab3349574cbb37283fa5e
Asep Khairul Anam <68137479+asepkh@users.noreply.github.com>
2020-11-02 11:47:39 +0700

    Update 3-create-collection.js

--- migrations/3-create-collection.js
+++ migrations/3-create-collection.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      literaturesId: {
+      literatureId: {
         type: Sequelize.INTEGER,
         references: {
           model: "Literatures",
