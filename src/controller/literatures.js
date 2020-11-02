733966ea88ff8bc0da426933e27a4d3ce059a32f
asepkh <asep.naxbkz@gmail.com>
2020-11-02 14:54:57 +0700

    Try fix

--- src/controller/literatures.js
+++ src/controller/literatures.js
@@ -24,9 +24,13 @@ exports.get = async (req, res) => {
     const literatureQuery = {
       where: {
         [Op.and]: [
-          sequelize.where(sequelize.fn("YEAR", sequelize.col("publication")), {
-            [Op.between]: [from || "0", to || "2020"],
-          }),
+          // sequelize.where(sequelize.fn("YEAR", sequelize.col("publication")), {
+          sequelize.where(
+            sequelize.literal("TO_CHAR(\"publication\", 'YYYY')"),
+            {
+              [Op.between]: [from || "0", to || "2020"],
+            }
+          ),
           {
             title: {
               [Op.like]: "%" + (q || "") + "%",
