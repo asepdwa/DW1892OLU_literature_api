859255fc0c7916add50483da7747a7e49e1c31e7
asepkh <asep.naxbkz@gmail.com>
2020-11-12 19:38:19 +0700

    try firebase

--- migrations/3-create-collection.js
+++ migrations/3-create-collection.js
@@ -14,6 +14,8 @@ module.exports = {
           model: "Literatures",
           key: "id",
         },
+        onUpdate: "SET NULL",
+        onDelete: "SET NULL",
       },
       UserId: {
         type: Sequelize.INTEGER,
@@ -21,6 +23,8 @@ module.exports = {
           model: "Users",
           key: "id",
         },
+        onUpdate: "SET NULL",
+        onDelete: "SET NULL",
       },
       createdAt: {
         allowNull: true,
