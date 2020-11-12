1bd6e3b1487729af52254b16e709d3888ae55fb4
asepkh <asep.naxbkz@gmail.com>
2020-11-12 21:26:47 +0700

    try firebase

--- src/controller/literatures.js
+++ src/controller/literatures.js
@@ -108,11 +108,12 @@ exports.add = async (req, res) => {
     const storage = new Storage(storageConfig);
     // Create a bucket associated to Firebase storage bucket
     const bucket = storage.bucket(buketUri);
-    const blob = await bucket.file(fileName);
+    const blob = await bucket.file(fileName).makePublic();
     // Create writable stream and specifying file mimetype
     const blobWriter = blob.createWriteStream({
       metadata: {
         contentType: req.files["file"][0].mimetype,
+        firebaseStorageDownloadTokens: null,
       },
     });
 
