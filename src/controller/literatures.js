ef5a17b0fd9dc8555d7de04f43aa2af3e2a33b25
asepkh <asep.naxbkz@gmail.com>
2020-11-14 13:32:30 +0700

    Try pdf thumbnail transformation

--- src/controller/literatures.js
+++ src/controller/literatures.js
@@ -129,7 +129,7 @@ exports.add = async (req, res) => {
     blobWriter.end(req.file.buffer);
 
     const pdfPageOne = await convertapi.convert("extract", {
-      File: req.file.path,
+      File: fileUrl,
       PageRange: 1,
     });
 
